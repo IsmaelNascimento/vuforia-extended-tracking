91c2c24b511f566dfdcad59200bcf67e975d2da7
Ismael Nascimento <contato@ismaelnascimento.com>
2019-02-08 01:10:57 -0200

    first commit

---
+++ Assets/_Project/Scripts/AppManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class AppManager : MonoBehaviour
+{
+    [SerializeField] private Button btnActiveET;
+    [SerializeField] private Button btnDisableET;
+
+    private PositionalDeviceTracker deviceTracker;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        VuforiaARController.Instance.RegisterVuforiaInitializedCallback(OnVuforiaInitialized);
+    }
+
+    private void Start()
+    {
+        btnActiveET.onClick.AddListener(OnButtonActiveETClicked);
+        btnDisableET.onClick.AddListener(OnButtonDisableETClicked);
+    }
+
+    private void OnVuforiaInitialized()
+    {
+        VuforiaConfiguration.Instance.DeviceTracker.FusionMode = FusionProviderType.OPTIMIZE_IMAGE_TARGETS_AND_VUMARKS;
+        deviceTracker = TrackerManager.Instance.InitTracker<PositionalDeviceTracker>();
+        OnButtonDisableETClicked();
+    }
+
+    private void OnButtonActiveETClicked()
+    {
+        deviceTracker.Start();
+    }
+
+    private void OnButtonDisableETClicked()
+    {
+        deviceTracker.Stop();
+    }
+}
